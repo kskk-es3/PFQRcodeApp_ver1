9014b9329d8e3cbf407e2a4572b17cb528b99128
Mitsui311 <231t257t@stu.kobe-u.ac.jp>
2025-01-08 23:38:45 +0900

    Update

--- app/app.py
+++ app/app.py
@@ -79,8 +79,6 @@ def download_zip():
 
 if __name__ == "__main__":
 
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
     # javaファイルを実行するためにJpypeを起動
     jpype.startJVM()
     # app中のjavaディレクトリをクラスパスに追加
