9d7388457e4b81d8c4f840cf461ebc691ceacb25
Mitsui311 <231t257t@stu.kobe-u.ac.jp>
2025-01-20 17:50:44 +0900

    ファインダーパターンの背景をランダムにならないようにした

--- app/HiddenqrPattern1.py
+++ app/HiddenqrPattern1.py
@@ -16,42 +16,20 @@ thickness = 14
 def HiddenQRgen(backimage, qrimage):           
 
     q_height, q_width = qrimage.shape[:2]
-    b_height, b_width = backimage.shape[:2]
+    b_height, b_width = backimage.shape[:2] 
 
+    #qrimageの余白ファインダパターンの以外の各モジュールを縦線の縞模様にしてbackimageに転写
+    for i in range (margin+modsize//2, b_width-margin, modsize):
+        heightcount = margin
+        while heightcount < b_height - margin:
+            if not ((margin <= i and i <= modsize * 8 + margin and margin <= heightcount and heightcount <= modsize * 7 + margin) or \
+            (q_height - modsize * 8 - margin <= i and i <= q_height - margin and margin <= heightcount and heightcount <= modsize * 7 + margin) or \
+            (margin <= i and i <= modsize * 8 + margin and q_width - modsize * 8 - margin <= heightcount and heightcount <= q_width - margin)):
 
-    ##モジュール模様作成(ファインダパターン部分の背景を作るため)
-    modimage = np.ones((b_height, b_width, 3),np.uint8)*255
+                patterncolor = qrimage[heightcount][i].tolist()
 
-    for i in range(0, b_height, modsize):
-        for j in range(0, b_width, modsize):
-            randcolor = randint(0, 1)
-            color = [0,255]
-            for x in range(i, i + modsize):
-                for y in range(j, j + modsize):
-                    modimage[x][y] = color[randcolor]
-
-    #機能パターン以外のQRコード部分をモジュール模様に重ねる
-    for i in range (0, q_height, modsize):
-        for j in range (0, q_width, modsize):
-            if not ((margin <= i and i <= modsize * 7 + margin and margin <= j and j <= modsize * 7 + margin) or \
-            (q_height - modsize * 8 - margin <= i and i <= q_height - margin and margin <= j and j <= modsize * 7 + margin) or \
-            (margin <= i and i <= modsize * 7 + margin and q_width - modsize * 8 - margin <= j and j <= q_width - margin)):
-                for x in range (i, i + modsize):
-                    for y in range (j, j + modsize):
-                        modimage[x][y] = [qrimage[x][y][0], qrimage[x][y][1], qrimage[x][y][2]]  
-
-
-
-    #modimageの各モジュールを縦線の縞模様にしてbackimageに転写
-    for i in range (modsize//2, b_width, modsize):
-        heightcount = 0
-        while heightcount < b_height:
-
-            patterncolor = [0,0,0]
-            patterncolor = modimage[heightcount][i].tolist()
-
-            if (modimage[heightcount][i] == patterncolor).all():
-                backimage[heightcount: heightcount + modsize, int(i - thickness/2): int(i + thickness/2) + 1,:] = patterncolor
+                if (qrimage[heightcount][i] == patterncolor).all():
+                    backimage[heightcount: heightcount + modsize, int(i - thickness/2): int(i + thickness/2) + 1,:] = patterncolor
             heightcount += modsize
 
 
@@ -69,24 +47,6 @@ def HiddenQRgen(backimage, qrimage):
             (0 <= i and i < modsize * 8 + margin and q_width - modsize * 8 - margin <= j and j < q_width):
                 if findpattern[i][j][3] == 255:
                     backimage[i][j] = [findpattern[i][j][0], findpattern[i][j][1], findpattern[i][j][2]]
-
-    # # 余白を追加
-    if not margin == 0:
-        for i in range (0, q_height):
-            for j in range (0, margin):
-                backimage[i,j] = 255
-
-        for i in range (0, margin):
-            for j in range (0, q_width):
-                backimage[i,j] = 255
-
-        for i in range (0, q_height):
-            for j in range (q_width - margin, q_width):
-                backimage[i,j] = 255
-
-        for i in range (q_height - margin, q_height):
-            for j in range (0, q_width):
-                backimage[i,j] = 255 
                     
     cv2.imwrite("Hiddenqr.bmp", backimage)
 
