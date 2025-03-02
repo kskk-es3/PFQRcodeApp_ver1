e623db53604f49152a867774585e9fde2f1d5bcc
kskk-es3 <kskk.es3@gmail.com>
2025-03-02 21:54:42 +0900

    Windows環境に対応

--- pysetup.sh
+++ pysetup.sh
@@ -12,3 +12,14 @@ pip install --upgrade pip
 pip install wheel
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+
+# If there's an error in the 'activate' step, create the environment manually
+if [ ! -d "venv" ]; then
+    echo "Virtual environment creation failed. Trying again..."
+    python -m venv venv
+    source venv/Scripts/activate  # Windows対応
+    pip install --upgrade pip
+    pip install wheel
+    pip install --upgrade pip setuptools wheel
+    pip install -r requirements.txt
+fi
