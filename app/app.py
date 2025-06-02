3ae916c96dbbcd5eb4f39c712bd9f0e042e8a4d6
kskk-es3 <129812637+kskk-es3@users.noreply.github.com>
2025-06-02 22:36:17 +0900

    [fix]

--- app/app.py
+++ app/app.py
@@ -8,49 +8,36 @@ app = Flask(__name__, static_folder='imagedata_output')
 
 # アップロードされたファイルを保存するディレクトリ
 UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create the folder if it doesn't exist
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-@app.route('/', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    new_filename = secure_filename(file.filename)
-    file.save(os.path.join(UPLOAD_FOLDER, new_filename))
-    return 'File uploaded successfully'
 
 # 許可するファイルの拡張子
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'bmp'}
 
 def allowed_file(filename):
     return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-
-        # ファイルがリクエストに含まれているかを確認
         if 'file' not in request.files:
             return redirect(request.url)
-        
+
         file = request.files['file']
 
-        # ファイルが選択されているかを確認
         if file.filename == '':
             return redirect(request.url)
 
-        # 許可されたファイル形式かを確認
         if file and allowed_file(file.filename):
-            new_filename = f"picture.png"
+            new_filename = "picture.png"
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
             input_text = request.form.get('text_input')
             input_picturesize = request.form.get('number')
 
-            #PFQRを生成
-            PFQRmain.PFQRmain(input_text,input_picturesize)
+            # PFQRを生成
+            PFQRmain.PFQRmain(input_text, input_picturesize)
 
-            # 処理完了後、結果ページへリダイレクト
             return redirect(url_for('result'))
 
     return render_template('index.html')
@@ -58,31 +45,23 @@ def upload_file():
 
 @app.route('/result')
 def result():
-    #結果画面に遷移
     return render_template('result.html')
 
 @app.route('/download/PFQRcode')
 def download_PFQRcode():
-    # PFQRcodeをダウンロード
     return send_from_directory('./imagedata_output', 'PFQRcode.jpg', as_attachment=True)
 
 @app.route('/download/PFQRcode_nomalFP')
 def download_PFQRcode_nomalFP():
-    # PFQRcode_nomalFPをダウンロード
     return send_from_directory('./imagedata_output', 'PFQRcode_nomalFP.jpg', as_attachment=True)
 
 @app.route('/download/pictureQRcode')
 def download_pictureQRcode():
-    # pictureQRcodeをダウンロード
     return send_from_directory('./imagedata_output', 'pictureQRcode.jpg', as_attachment=True)
 
 
 if __name__ == "__main__":
-
-    # javaファイルを実行するためにJpypeを起動
     jpype.startJVM()
-    # app中のjavaディレクトリをクラスパスに追加
     jpype.addClassPath("java")
-    app.run(host = "0.0.0.0", port = 80, debug = False)
-    # Jpypeを終了
+    app.run(host="0.0.0.0", port=80, debug=False)
     jpype.shutdownJVM()
