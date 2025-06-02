d70d446d50b289e23abadd98e4c75c37544441c1
kskk-es3 <129812637+kskk-es3@users.noreply.github.com>
2025-06-02 21:24:46 +0900

    [fix]

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cleanup() {
+    rm -rf ./php/output
+    docker container prune -f
+    docker image prune  -f
+}
+trap cleanup EXIT
+
+# docker compose build --no-cache
+# docker compose up -d --force-recreate
+
+docker compose build
+docker compose up
